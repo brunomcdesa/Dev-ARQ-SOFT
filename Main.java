3052e032d3c4456c08ed679fd846f5ce812dc4dc
brunomcdesa <102191355+brunomcdesa@users.noreply.github.com>
2022-09-02 21:30:43 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,39 @@
+
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 
-public class Main {
-	public static void main( String[] args) {
-		GerenciadorDeArquivos gr = new GerenciadorDeArquivos();
-		File f = gr.openFile();
-		System.out.println("Arquivo selecionado: " + f);
-		ArrayList<String> lista = gr.readFile(f);
-		for(String item : lista) {
-			System.out.println("--- " + item );
-		}
-		
-	}
+/**
+ *
+ * @author julianacostasilva
+ */
+public class ArquivosDeTexto {
+
+    public static void main(String[] args) {
+    	
+        System.out.println("Hello World!");
+        GerenciadorDeArquivo gr = new GerenciadorDeArquivo();
+        File f = gr.openFile();
+        System.out.println("Arquivo selecionado: " + f);
+        ArrayList<String> linhas = gr.lerArquivo(f);
+        
+        ArrayList<ClientePF> clientes = new ArrayList<ClientePF>();
+      
+        System.out.println("Cliente ou conta?");
+        
+        ClientePF c1 = new ClientePF();
+        
+        for(String item : linhas){
+        	String[] vetorItem = item.split(",");
+        	c1.setNome(vetorItem[0]);
+        	c1.setEmail(vetorItem[1]);
+        	c1.setTelefone(vetorItem[2]);
+        	c1.setCpf(vetorItem[3]);
+        	c1.setNascimento(vetorItem[4]);
+        	
+            //System.out.println("\n"+c1.toString());
+            clientes.add(c1);
+            System.out.println(clientes);
+        }
+    }
 }
